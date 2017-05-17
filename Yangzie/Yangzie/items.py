329ccc1fd25c1b771a092783a05b15ce1dc5292e
杨国奇 <lnmput@gmail.com>
2017-05-17 09:51:29 +0800

    规范数据

--- Yangzie/Yangzie/items.py
+++ Yangzie/Yangzie/items.py
@@ -9,6 +9,9 @@ import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst
 from scrapy.loader import ItemLoader
 from w3lib.html import remove_tags
+import re
+
+from scrapy.loader.processors import Join
 
 class YangzieItem(scrapy.Item):
     # define the fields for your item here like:
@@ -25,6 +28,26 @@ class LagouItemLoader(ItemLoader):
 # 去掉城市名两边的斜线
 def remove_sp(value):
     return value.replace("/", "")
+# 去掉 \n
+
+def remove_n(value):
+    return value.replace('\n', "")
+
+# 去掉space
+def remove_space(value):
+    return value.replace(" ", "")
+
+def get_default(value):
+    return value
+
+# 查看地图
+def remove_map(value):
+    if re.match('.*(查看地图).*', value):
+        return value.replace('查看地图', '')
+
+
+
+
 
 class LagouItem(scrapy.Item):
     url = scrapy.Field()
@@ -45,11 +68,14 @@ class LagouItem(scrapy.Item):
     )
     publish_time = scrapy.Field()
     tags = scrapy.Field(
-       # input_processor = Join(",")
+        # input_processor = MapCompose(list_to_str),
+        output_processor=Join(separator=u' ')
     )
-    descs = scrapy.Field()
+    descs = scrapy.Field(
+        input_processor=MapCompose(remove_tags, remove_n, remove_space)
+    )
     address = scrapy.Field(
-        input_processor = MapCompose(remove_tags)
+        input_processor=MapCompose(remove_tags, remove_n, remove_space, remove_map)
     )
     company_url = scrapy.Field()
     company_name = scrapy.Field()
